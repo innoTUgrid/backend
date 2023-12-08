8321a86aff99e151b50401bf3bbf51491ff6ce49
nymphbox <nymphbox@proton.me>
2023-12-08 11:19:12 +0100

    add migrations for emission factor table
    the emission factor table contains data from various sources on emission factors by energy carrier. this table can either be populated by scrapers or manually through the frontend. the source column allows to group various factors and make the emission calculation dynamic, given a set of emission factors from one particular source. for now, I made the choice to not denominate the corresponding greenhouse gas for the emission specifically, but rather indirectly through the unit. this might be useful to change later, depending on requirements.

---
+++ migrations/20231206125747_create_table_for_emission_factors_by_carrier.down.sql
@@ -0,0 +1,2 @@
+-- drop the emission factors table relates various emission factors from various sources to an energy carrier
+drop table if exists emission_factor;
---
+++ migrations/20231206125747_create_table_for_emission_factors_by_carrier.up.sql
@@ -0,0 +1,13 @@
+-- remove the emission factor table
+create table if not exists emission_factor (
+   id serial primary key,
+   carrier integer references energy_carrier(id) not null,
+   factor numeric not null,
+   unit text not null,
+   source text not null,
+   source_url text,
+   created_at timestamptz not null default now(),
+   updated_at timestamptz not null default now()
+);
+select trigger_updated_at('emission_factor');
+
