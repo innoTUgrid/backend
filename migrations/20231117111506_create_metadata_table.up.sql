afebc9342a0a3f197a1c799dd6e9c2f9d13e48fe
nymphbox <nymphbox@proton.me>
2023-12-08 11:15:37 +0100

    modify the metadata table creation migration to use new energy carrier lookup table
    we previously used a string based energy carrier column, which now changes to a reference to a lookup table for a variety of energy carriers used by SMARD, which is also where we get energy prices and proportions in the total energy production. this will later on simplify emission and other kpi calculations

--- migrations/20231117111506_create_metadata_table.up.sql
+++ migrations/20231117111506_create_metadata_table.up.sql
@@ -1,9 +1,35 @@
--- Add up migration script here
+-- create the energy carrier lookup table
+
+create table if not exists energy_carrier (
+   id serial primary key,
+   name text unique not null,
+   created_at timestamptz not null default now(),
+   updated_at timestamptz not null default now()
+);
+select trigger_updated_at('energy_carrier');
+
+-- insert predefined energy carriers used by SMARD
+insert into energy_carrier (name) values ('coal'),
+                                  ('lignite'),
+                                  ('oil'),
+                                  ('gas'),
+                                  ('nuclear'),
+                                  ('solar'),
+                                  ('hydro'),
+                                  ('biomass'),
+                                  ('onwind'),
+                                  ('offwind'),
+                                  ('pumped_storage'),
+                                  ('other_renewable'),
+                                  ('other_conventional');
+
+
+-- create the timeseries metadata tables
 create table if not exists meta (
     id serial primary key,
     identifier text collate "case_insensitive" not null,
     unit text collate "case_insensitive" not null,
-    carrier text,
+    carrier integer references energy_carrier(id),
     consumption boolean,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now(),
