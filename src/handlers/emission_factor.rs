4210a5c6acf95951f1a0430b81ef8c8d41f0a2d7
nymphbox <nymphbox@proton.me>
2024-02-04 19:20:32 +0100

    add read and write endpoints for emission factors

--- src/handlers/emission_factor.rs
+++ src/handlers/emission_factor.rs
@@ -1,22 +1,64 @@
-use crate::models::Result;
-use crate::{error::ApiError, models::EmissionFactor};
-use axum::extract::State;
-use sqlx::{Pool, Postgres};
-
+use axum::extract::{Query, State};
 use axum::Json;
+use sqlx::{Pool, Postgres};
+use crate::error::ApiError;
+use crate::models::{ CreateEmissionFactorRequest, CreateEmissionFactorResponse, EmissionFactor, EmissionFactorFilter};
 
-pub async fn get_emission_factors(
+pub async fn add_emission_factor(
     State(pool): State<Pool<Postgres>>,
-) -> Result<Json<Vec<EmissionFactor>>, ApiError> {
-    let emission_factors = sqlx::query_as!(
-      EmissionFactor,
-      r#"
-        SELECT emission_factor.id, energy_carrier.name as carrier, factor, unit, source, source_url from emission_factor
-        JOIN energy_carrier ON emission_factor.carrier = energy_carrier.id
-        "#,
-    )
-    .fetch_all(&pool)
-    .await?;
+    request: Json<CreateEmissionFactorRequest>
+) -> Result<Json<CreateEmissionFactorResponse>, ApiError> {
 
-    Ok(Json(emission_factors))
+
+    let emission_factor: CreateEmissionFactorResponse= sqlx::query_as!(
+        CreateEmissionFactorResponse,
+        r"
+        insert into emission_factor
+        (factor, unit, carrier, source, source_url)
+        select $1, $2, energy_carrier.id, $4, $5
+        from energy_carrier
+        where energy_carrier.name = $3
+        returning id, factor, unit, $3 as carrier, source, $5 as source_url
+        ",
+        request.factor,
+        request.unit,
+        request.carrier,
+        request.source,
+        request.source_url.as_deref(),
+        )
+        .fetch_one(&pool)
+        .await?;
+
+    Ok(Json(emission_factor))
+}
+
+pub async fn get_emission_factor(
+    State(pool): State<Pool<Postgres>>,
+    Query(filter): Query<EmissionFactorFilter>
+) -> Result<Json<Vec<EmissionFactor>>, ApiError> {
+
+    let factors = sqlx::query_as!(
+        EmissionFactor,
+        r"
+        select
+            emission_factor.id as id,
+            factor,
+            unit,
+            energy_carrier.name as carrier,
+            source,
+            source_url,
+            emission_factor.updated_at as updated_at,
+            emission_factor.created_at as created_at
+        from emission_factor
+            join energy_carrier on emission_factor.carrier = energy_carrier.id
+        where
+            ($1::text is null or emission_factor.source = $1) and
+            ($2::text is null or energy_carrier.name = $2)
+        ",
+        filter.source.as_deref(),
+        filter.carrier.as_deref(),
+    ).fetch_all(&pool)
+        .await?;
+
+    Ok(Json(factors))
 }
