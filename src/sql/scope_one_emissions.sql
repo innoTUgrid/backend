c9c5f0e6ad40cd1dcc0a567d43a8c0e08f310bd1
nymphbox <nymphbox@proton.me>
2024-02-12 18:23:07 +0100

    add proper kwh calculation and refactor sql query for scope one emissions

--- src/sql/scope_one_emissions.sql
+++ src/sql/scope_one_emissions.sql
@@ -1,28 +1,51 @@
--- local production
+with local_production as (
+    select
+        ts.series_timestamp as timestamp,
+        ts.series_value as value,
+        meta.identifier as identifier,
+        energy_carrier.name as energy_carrier,
+        emission_factor.factor as emission_factor,
+        emission_factor.unit as emission_factor_unit,
+        extract(epoch from (ts.series_timestamp - lag(ts.series_timestamp) over (order by ts.series_timestamp))) / 3600 as timestamp_distance
+    from ts
+        join meta on ts.meta_id = meta.id
+        join energy_carrier on meta.carrier = energy_carrier.id
+        join emission_factor on energy_carrier.id = emission_factor.carrier
+    where
+        meta.consumption = false
+        and ts.series_timestamp between $1 and $2
+), kwh as (
+    select
+        timestamp,
+        value * timestamp_distance as production,
+        identifier,
+        energy_carrier,
+        emission_factor,
+        emission_factor_unit
+    from local_production
+), production_with_emissions as (
+     select
+         time_bucket($3::interval, kwh.timestamp) as bucket,
+         kwh.identifier                           as source_of_production,
+         kwh.energy_carrier                       as production_carrier,
+         sum(greatest(kwh.production, 0.0))       as production,
+         kwh.emission_factor                      as emission_factor,
+         kwh.emission_factor_unit                 as emission_factor_unit
+     from kwh
+     group by
+         kwh.energy_carrier,
+         kwh.identifier,
+         time_bucket($3::interval, kwh.timestamp),
+         kwh.emission_factor,
+         kwh.emission_factor_unit
+ )
 select
-    --time_bucket('{interval}', ts.series_timestamp, ) as bucket,
-    time_bucket('{interval}', ts.series_timestamp, origin=>$1::timestamptz) as bucket,
-    meta.identifier as source_of_production,
-    energy_carrier.name as production_carrier,
-    -- average production per identifier over interval to account for KwH
-    avg(greatest(ts.series_value, 0.0)) as production,
-    meta.unit as production_unit,
-    -- scope 1 emissions
-    (avg(greatest(ts.series_value, 0.0)) * avg(emission_factor.factor)) as scope_1_emissions,
-    emission_factor.unit as emission_factor_unit
-from ts
-    join meta on ts.meta_id = meta.id
-    join energy_carrier on meta.carrier = energy_carrier.id
-    join emission_factor on energy_carrier.id = emission_factor.carrier
-where
-    meta.consumption = false AND
-    meta.local = true
-    AND
-    ts.series_timestamp between $1 and $2
-group by
-    bucket,
-    source_of_production,
-    production_carrier,
-    production_unit,
-    emission_factor_unit
+    production_with_emissions.production * production_with_emissions.emission_factor as scope_one_emissions,
+    production_with_emissions.bucket as bucket,
+    production_with_emissions.source_of_production as source_of_production,
+    production_with_emissions.production_carrier as production_carrier,
+    production_with_emissions.production as production,
+    production_with_emissions.emission_factor_unit
+from
+    production_with_emissions
 order by bucket
