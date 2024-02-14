7cfbbeac23b6358da3f0d0239b53fb2accfde651
Ole Fass <jan.ole.fass@gmail.com>
2024-02-14 18:52:13 +0100

    fixed kWh calculations

--- src/sql/scope_one_emissions.sql
+++ src/sql/scope_one_emissions.sql
@@ -1,49 +1,57 @@
--- 
-WITH aggregated_data AS (
-    SELECT
-        time_bucket($3::interval, ts.series_timestamp) AS bucket,
-        meta.identifier as source_of_production,
-        energy_carrier.name as production_carrier,
-        -- aggregations
-        avg(greatest(ts.series_value, 0.0)) AS production_mean,
-        sum(greatest(ts.series_value, 0.0)) AS production_sum,
-        NULLIF(COUNT(ts.series_value) FILTER (WHERE ts.series_value > 0),0) AS nr_of_records,
-        EXTRACT(epoch FROM $3::interval) / 3600 AS nr_of_hours,
-        -- average result equals actual emission factor for energy_carrier used by source_of_production
-        avg(emission_factor.factor) AS emission_factor,
-        emission_factor.unit AS emission_factor_unit,
-        meta.unit as production_unit
+with local_production as (
+    select
+        ts.series_timestamp as timestamp,
+        ts.series_value as value,
+        meta.identifier as identifier,
+        energy_carrier.name as energy_carrier,
+        emission_factor.factor as emission_factor,
+        emission_factor.unit as emission_factor_unit,
+        -- catch case that first entry is NULL
+        CASE
+            WHEN LAG(ts.series_timestamp) OVER (PARTITION BY ts.meta_id ORDER BY ts.series_timestamp) IS NOT NULL 
+            THEN extract(epoch from (ts.series_timestamp - lag(ts.series_timestamp) over (PARTITION BY ts.meta_id order by ts.series_timestamp))) / 3600
+            ELSE extract(epoch from (LEAD(ts.series_timestamp) OVER (PARTITION BY ts.meta_id order by ts.series_timestamp)) - ts.series_timestamp) / 3600
+        END AS timestamp_distance
     from ts
         join meta on ts.meta_id = meta.id
         join energy_carrier on meta.carrier = energy_carrier.id
         join emission_factor on energy_carrier.id = emission_factor.carrier
     where
-        meta.consumption = false 
-        AND
+        meta.consumption = false and
         meta.local = true
-        AND
-        ts.series_timestamp between $1 and $2
-    group by
-        bucket,
-        source_of_production,
-        production_carrier,
-        production_unit,
+        and ts.series_timestamp between $1 and $2
+), kwh as (
+    select
+        timestamp,
+        value * timestamp_distance as production,
+        identifier,
+        energy_carrier,
+        emission_factor,
         emission_factor_unit
-    order by bucket
+    from local_production
+), production_with_emissions as (
+    select
+        time_bucket($3::interval, kwh.timestamp) as bucket,
+        kwh.identifier                           as source_of_production,
+        kwh.energy_carrier                       as production_carrier,
+        sum(greatest(kwh.production, 0.0))       as production,
+        kwh.emission_factor                      as emission_factor,
+        kwh.emission_factor_unit                 as emission_factor_unit
+    from kwh
+    group by
+        time_bucket($3::interval, kwh.timestamp),
+        kwh.energy_carrier,
+        kwh.identifier,
+        kwh.emission_factor,
+        kwh.emission_factor_unit
 )
-SELECT 
-    bucket,
-    source_of_production,
-    production_carrier,
-    --production_unit,
-    -- average kwh produced in time bucket
-    production_sum / nr_of_records / nr_of_hours AS production,
-    (production_sum / nr_of_records) / nr_of_hours * emission_factor AS scope_one_emissions,
-    emission_factor_unit
-    --source_of_production,
-    --production_carrier,
-    --production_mean,
-    --production_sum,
-    --nr_of_records,
-    --nr_of_hours,
-FROM aggregated_data;
+select
+    production_with_emissions.production * production_with_emissions.emission_factor as scope_one_emissions,
+    production_with_emissions.bucket as bucket,
+    production_with_emissions.source_of_production as source_of_production,
+    production_with_emissions.production_carrier as production_carrier,
+    production_with_emissions.production as production,
+    production_with_emissions.emission_factor_unit
+from
+    production_with_emissions
+order by bucket;
