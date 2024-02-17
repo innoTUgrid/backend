4d014ba95e27fa5815c6d6adc1e73b278ec3fe6d
nymphbox <nymphbox@proton.me>
2024-02-17 16:56:57 +0100

    specify binary in init db shell script

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -7,4 +7,4 @@ cargo install --version=0.7.3 sqlx-cli --locked --no-default-features --features
 sqlx migrate run
 
 # import inno2grid data (init only)
-LOAD_INITIAL_DATA_PATH=assets/inno2grid_all_data_cleaned_and_aligned.meta.yaml cargo run -- init
+LOAD_INITIAL_DATA_PATH=assets/inno2grid_all_data_cleaned_and_aligned.meta.yaml cargo run --bin inno2grid-backend -- init
