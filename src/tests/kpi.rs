cc77e743780c4ff722188c2c764c1fe9953fdc37
nymphbox <nymphbox@proton.me>
2024-02-18 21:03:58 +0100

    add additional test to check if emissions sum

--- src/tests/kpi.rs
+++ src/tests/kpi.rs
@@ -118,3 +118,41 @@ async fn test_scope_one_plus_two_eq_total() {
     // we need to floor the values because the sum of the scopes might not be exactly the same as the total
     assert_eq!((sum_scope_one + sum_scope_two).floor(), body.value.floor());
 }
+#[tokio::test]
+async fn test_scoped_emission_sum() {
+    let client = get_client().await;
+
+    let response_scope_two = client
+        .get("/v1/kpi/scope_two_emissions/?interval=1year&source=IPCC")
+        .send()
+        .await;
+
+    assert!(response_scope_two.status().is_success());
+    let body_scope_two: Vec<EmissionsByCarrier> = response_scope_two.json().await;
+
+    let response_scope_one = client
+        .get("/v1/kpi/scope_one_emissions/?interval=1year&source=IPCC")
+        .send()
+        .await;
+
+    assert!(response_scope_one.status().is_success());
+    let body_scope_one: Vec<EmissionsByCarrier> = response_scope_one.json().await;
+
+    let response = client
+        .get("/v1/kpi/total_co2_emissions/?interval=1year")
+        .send()
+        .await;
+
+    assert!(response.status().is_success());
+    let body: KpiResult = response.json().await;
+
+    let sum_scope_one = body_scope_one
+        .iter()
+        .fold(0.0, |acc, x| acc + x.value.unwrap());
+    let sum_scope_two = body_scope_two
+        .iter()
+        .fold(0.0, |acc, x| acc + x.value.unwrap());
+
+    // we need to floor the values because the sum of the scopes might not be exactly the same as the total
+    assert_eq!((sum_scope_one + sum_scope_two).floor(), body.value.floor());
+}
